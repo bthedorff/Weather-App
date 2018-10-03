9d141034641ab4dc043660cfd3b304d6735e4c0d
bthedorff <32941719+bthedorff@users.noreply.github.com>
2018-10-03 15:33:24 -0400

    Allendale test

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,9 @@ import tk.plogitech.darksky.forecast.*;
 //test
 public class Main {
 	public static void main(String args[]) throws ForecastException {
-	ForecastRequest request = new ForecastRequestBuilder()
+		ForecastRequest request = new ForecastRequestBuilder()
 	        .key(new APIKey("5f3d14bc52bfc3ec10922d31be0e7e61"))
-	        .location(new GeoCoordinates(new Longitude(13.377704), new Latitude(52.516275))).build();
+	        .location(new GeoCoordinates(new Longitude(-85.886879), new Latitude(42.966679))).build();
 
 	    DarkSkyClient client = new DarkSkyClient();
 	    String forecast = client.forecastJsonString(request);
