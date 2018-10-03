0eb779e87942528f5159048b4cbd892722e7f35e
bthedorff <32941719+bthedorff@users.noreply.github.com>
2018-10-03 16:47:38 -0400

    Added API Comment Don't Delete

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import tk.plogitech.darksky.forecast.model.Latitude;
 import tk.plogitech.darksky.forecast.model.Longitude;
 import tk.plogitech.darksky.forecast.*;
 
-
+// API Key   5f3d14bc52bfc3ec10922d31be0e7e61
 //test
 public class Main {
 	public static void main(String args[]) throws ForecastException {
