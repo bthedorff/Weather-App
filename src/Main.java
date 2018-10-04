64a22e23d75629381958bff1eea02d78abc32474
bthedorff <32941719+bthedorff@users.noreply.github.com>
2018-10-04 15:28:22 -0400

    New example test

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,34 @@
 import tk.plogitech.darksky.forecast.model.Latitude;
 import tk.plogitech.darksky.forecast.model.Longitude;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
 import tk.plogitech.darksky.forecast.*;
 
 // API Key   5f3d14bc52bfc3ec10922d31be0e7e61
 //test
 public class Main {
 	public static void main(String args[]) throws ForecastException {
-		ForecastRequest request = new ForecastRequestBuilder()
-	        .key(new APIKey("5f3d14bc52bfc3ec10922d31be0e7e61"))
-	        .location(new GeoCoordinates(new Longitude(-85.886879), new Latitude(42.966679))).build();
+//		ForecastRequest request = new ForecastRequestBuilder()
+//	        .key(new APIKey("5f3d14bc52bfc3ec10922d31be0e7e61"))
+//	        .location(new GeoCoordinates(new Longitude(-85.886879), new Latitude(42.966679))).build();
+//
+//	    DarkSkyClient client = new DarkSkyClient();
+//	    String forecast = client.forecastJsonString(request);
+//	    System.out.println(forecast);
+		
+	    ForecastRequest request = new ForecastRequestBuilder()
+	            .key(new APIKey("5f3d14bc52bfc3ec10922d31be0e7e61"))
+	            .time(Instant.now().minus(5, ChronoUnit.DAYS))
+	            .language(ForecastRequestBuilder.Language.de)
+	            .units(ForecastRequestBuilder.Units.us)
+	            .exclude(ForecastRequestBuilder.Block.minutely)
+	            .extendHourly()
+	            .location(new GeoCoordinates(new Longitude(13.377704), new Latitude(52.516275))).build();
 
-	    DarkSkyClient client = new DarkSkyClient();
-	    String forecast = client.forecastJsonString(request);
-	    System.out.println(forecast);
-
+	        DarkSkyClient client = new DarkSkyClient();
+	        String forecast = client.forecastJsonString(request);
+	        System.out.println(forecast);
 	}
 }
