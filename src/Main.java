f8d5b8e97b4acb0e2d8191aa191e30f3183d3179
bthedorff <32941719+bthedorff@users.noreply.github.com>
2018-10-04 16:36:54 -0400

    API string

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,9 @@ import tk.plogitech.darksky.forecast.*;
 // API Key   5f3d14bc52bfc3ec10922d31be0e7e61
 //test
 public class Main {
+	private String APIKey;
 	public static void main(String args[]) throws ForecastException {
+		String APIKey = "5f3d14bc52bfc3ec10922d31be0e7e61";
 //		ForecastRequest request = new ForecastRequestBuilder()
 //	        .key(new APIKey("5f3d14bc52bfc3ec10922d31be0e7e61"))
 //	        .location(new GeoCoordinates(new Longitude(-85.886879), new Latitude(42.966679))).build();
@@ -25,10 +27,12 @@ public class Main {
 	            .units(ForecastRequestBuilder.Units.us)
 	            .exclude(ForecastRequestBuilder.Block.minutely)
 	            .extendHourly()
-	            .location(new GeoCoordinates(new Longitude(13.377704), new Latitude(52.516275))).build();
+	            .location(new GeoCoordinates(new Longitude(-85.886879), new Latitude(42.966679))).build();
 
 	        DarkSkyClient client = new DarkSkyClient();
 	        String forecast = client.forecastJsonString(request);
 	        System.out.println(forecast);
+		
+	        
 	}
 }
