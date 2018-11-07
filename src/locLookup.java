3f8baa48668fec4554d4f6b74f7bf224d6095ba7
dejongry <dejongr@mail.gvsu.edu>
2018-11-07 17:51:11 -0500

    locLookup
    Basic GUI if we do location lookup

--- src/locLookup.java
+++ src/locLookup.java
@@ -1,36 +1,42 @@
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import java.awt.event.ActionListener;
+import javax.swing.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class locLookup {
+	private JPanel Panel;
+	private JLabel location;
+	private JButton searchBut;
+	private JTextField userInput;
 
 	public void locationLookup() {
-		JFrame locFrame = new JFrame();
+		JFrame locFrame = new JFrame("Set Location");
+		locFrame.getContentPane();
 		locFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		locFrame.setTitle("Set Location");
-		locFrame.setSize(800, 650);
-		locFrame.setLocationRelativeTo(null);
 		
+		Panel = (JPanel) locFrame.getContentPane();
+		Panel.setLayout(null);
 		
-		JButton okBut = new JButton("OK");
-		okBut.setSize(40, 40);
-		okBut.addActionListener(new ActionListener(){
-
+		searchBut = new JButton("Search Location");
+		searchBut.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent e) {
-			//locFrame.setVisible(false);
-			//locFrame.dispose();
 				System.exit(0);
 			}
 		});
-		JTextField locTextField = new JTextField("",10);
-		//locFrame.add(locTextField);
-		locFrame.add(okBut);
+		userInput = new JTextField("",10);
+		location = new JLabel("Enter Desired Location:");
 		
+		searchBut.setBounds(160, 80, 150, 40);
+		userInput.setBounds(157, 20, 250, 40);
+		location.setBounds(10, 20, 250, 40);
+		Panel.add(searchBut);
+		Panel.add(userInput);
+		Panel.add(location);
+		locFrame.setSize(450, 150);
+		locFrame.setLocationRelativeTo(null);
 		locFrame.setVisible(true);
 	}
 	
