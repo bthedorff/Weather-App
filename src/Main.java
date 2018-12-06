c3d6a0187245db91a9ce4b446a6f591831118a89
bthedorff <32941719+bthedorff@users.noreply.github.com>
2018-12-06 18:44:01 -0500

    Testing

--- src/Main.java
+++ src/Main.java
@@ -21,7 +21,7 @@ public class Main {
 	
 	
 	
-	public static void main(String args[]) throws Exception {
+	public static void main(String args[]) {
 
 		
 		searchGUI sgui = new searchGUI();
---
+++ src/TestCases.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+public class TestCases {
+
+	@Test
+	public static void main(String args[]) {
+
+		
+			searchGUI sgui = new searchGUI();
+
+		}
+}
